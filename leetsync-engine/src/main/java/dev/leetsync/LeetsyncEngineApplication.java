6962dcbeef22d91cb3ed0c74648b992dba0219df
Aache_wale_bhaiya <sumanmandal8955@gmail.com>
2026-08-01 06:24:04 +0530

    chore: initialize Spring Boot 4.1 project

---
+++ leetsync-engine/src/main/java/dev/leetsync/LeetsyncEngineApplication.java
@@ -0,0 +1,13 @@
+package dev.leetsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetsyncEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetsyncEngineApplication.class, args);
+	}
+
+}
---
+++ leetsync-engine/src/test/java/dev/leetsync/LeetsyncEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leetsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetsyncEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
